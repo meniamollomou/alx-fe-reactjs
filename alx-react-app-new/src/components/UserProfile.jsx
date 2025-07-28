db58b8ef710098cabed5dca26158faf375a417d5
Menia Mollomou <mollomoumenia@gamil.com>
2025-07-29 05:07:11 +0800

    Created a new react App

--- alx-react-app-new/src/components/UserProfile.jsx
+++ alx-react-app-new/src/components/UserProfile.jsx
@@ -1,9 +1,11 @@
 const UserProfile = (props) => {
   return (
-    <div style={{ border: "1px solid #ccc", padding: "20px", maxWidth: "300px", margin: "20px auto", borderRadius: "10px", backgroundColor: "#fff" }}>
-      <h2>{props.name}</h2>
-      <p>Age: {props.age}</p>
-      <p>Bio: {props.bio}</p>
+    <div style={{ border: '1px solid gray', padding: '20px', margin: '20px auto', width: '300px', borderRadius: '10px', backgroundColor: '#fff' }}>
+      <h2 style={{ color: 'blue', fontSize: '22px', marginBottom: '10px' }}>{props.name}</h2>
+      <p style={{ fontSize: '16px' }}>
+        Age: <span style={{ fontWeight: 'bold', color: 'blue' }}>{props.age}</span>
+      </p>
+      <p style={{ fontSize: '16px', fontStyle: 'italic' }}>Bio: {props.bio}</p>
     </div>
   );
 };

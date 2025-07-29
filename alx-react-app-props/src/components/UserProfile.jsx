ec8dd6008ffa0cf840f9953a45e3308357249ab8
Menia Mollomou <mollomoumenia@gamil.com>
2025-07-29 23:57:14 +0800

    Modified App

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,15 +1,7 @@
-import { useContext } from 'react';
-import UserContext from '../UserContext';
+import UserDetails from './UserDetails';
 
-const UserProfile = () => {
-  const user = useContext(UserContext);
+function UserInfo() {
+  return <UserDetails />;
+}
 
-  return (
-    <div>
-      <h2>{user.name}</h2>
-      <p>Email: {user.email}</p>
-    </div>
-  );
-};
-
-export default UserProfile;
+export default UserInfo;

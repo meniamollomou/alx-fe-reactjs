c60ac48623af5304361bab734d4dae019f12258c
Menia Mollomou <mollomoumenia@gamil.com>
2025-07-29 23:20:14 +0800

    Modified App and UserContent

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,14 +1,9 @@
-import UserProfile from './components/UserProfile';
-import UserContext from './UserContext';
+import ProfilePage from './ProfilePage';
 
 function App() {
   const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
 
-  return (
-    <UserContext.Provider value={userData}>
-      <UserProfile />
-    </UserContext.Provider>
-  );
+  return <ProfilePage userData={userData} />;
 }
 
 export default App;
--- alx-react-app-props/src/UserInfo.jsx
+++ alx-react-app-props/src/UserInfo.jsx
@@ -1,8 +1,7 @@
-import React from 'react';
 import UserDetails from './UserDetails';
 
-function UserInfo() {
-  return <UserDetails />;
+function UserInfo({ userData }) {
+  return <UserDetails userData={userData} />;
 }
 
 export default UserInfo;

a418197df81164c960aca0f634528694e127fbfc
Menia Mollomou <mollomoumenia@gamil.com>
2025-07-30 11:57:39 +0800

    Working to handle issues

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,14 +1,9 @@
-import ProfilePage from './components/ProfilePage';
-import UserContext from './UserContext';
+import ProfilePage from './ProfilePage';
 
 function App() {
   const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
 
-  return (
-    <UserContext.Provider value={userData}>
-      <ProfilePage />
-    </UserContext.Provider>
-  );
+  return <ProfilePage userData={userData} />;
 }
 
 export default App;

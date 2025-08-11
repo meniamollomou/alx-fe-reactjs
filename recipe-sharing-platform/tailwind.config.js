0f7480b936c04c60a509b17f5a0b02074865b628
Menia Mollomou <mollomoumenia@gamil.com>
2025-08-11 15:58:16 +0800

    remodified tailwind.config.js

--- recipe-sharing-platform/tailwind.config.js
+++ recipe-sharing-platform/tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}"
+    "./index.html",               // Vite's root HTML file
+    "./src/**/*.{js,jsx,ts,tsx}"  // All source files
   ],
   theme: {
     extend: {},

d96f32a229caecadcdbc093d8949ba30e6ca8fa3
Menia Mollomou <mollomoumenia@gamil.com>
2025-08-25 01:00:50 +0800

    Created react-todo

--- react-todo/src/App.jsx
+++ react-todo/src/App.jsx
@@ -3,6 +3,8 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import TodoList from './components/TodoList'  // ✅ Import TodoList
+
 function App() {
   const [count, setCount] = useState(0)
 
@@ -25,6 +27,10 @@ function App() {
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
+
+      {/* ✅ Use TodoList here */}
+      <TodoList />
+
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
